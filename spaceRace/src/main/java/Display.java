01ad81915528835ff7d95c667389a879d5acbaaf
Hugo Castro <hreisc@gmail.com>
2022-01-03 01:00:04 -0100

    1 update screen

--- spaceRace/src/main/java/Display.java
+++ spaceRace/src/main/java/Display.java
@@ -1,2 +1,29 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
 public class Display {
+    private int width;
+    private int height;
+
+    public Display(int width, int height){
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#800000"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+
+    }
 }
--- spaceRace/src/main/java/Game.java
+++ spaceRace/src/main/java/Game.java
@@ -1,3 +1,60 @@
-public class Game {
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
+
+public class Game {
+    Screen screen = null;
+    Display display = new Display(100, 70);
+
+    public Game() {
+
+        try {
+            TerminalSize terminalSize = new TerminalSize(100, 70);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        public void processKey (KeyStroke key){
+            switch (key.getKeyType()) {
+                //case ArrowUp -> arena.moveHero(arena.moveUp());
+                //case ArrowDown -> arena.moveHero(arena.moveDown());
+                //case ArrowLeft -> arena.moveHero(arena.moveLeft());
+                //case ArrowRight -> arena.moveHero(arena.moveRight());
+            }
+
+        }
+
+        private void draw () throws IOException {
+            screen.clear();
+            display.draw(screen.newTextGraphics());
+            screen.refresh();
+        }
+
+        public void run () throws IOException {
+            while (true) {
+                draw();
+                KeyStroke key = screen.readInput();
+                processKey(key);
+
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                    screen.close();
+                }
+                if (key.getKeyType() == KeyType.EOF)
+                    break;
+            }
+        }
 }
