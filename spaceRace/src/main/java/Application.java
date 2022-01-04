076bcd1c4509ee1f725b91b52bedddd68940653f
Hugo Castro <hreisc@gmail.com>
2022-01-04 00:44:46 -0100

    updates

--- spaceRace/src/main/java/Application.java
+++ spaceRace/src/main/java/Application.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String args[]) throws IOException {
-        //System.out.println("Hello World");
         Game game = new Game();
         game.run();
     }
