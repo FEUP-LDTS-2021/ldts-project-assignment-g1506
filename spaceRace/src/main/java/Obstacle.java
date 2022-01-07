8b509178ae337b10fb96e3c44c58d29be9d7ca8b
Hugo Castro <hreisc@gmail.com>
2022-01-07 14:19:36 +0000

    Color obstacles

--- spaceRace/src/main/java/Obstacle.java
+++ spaceRace/src/main/java/Obstacle.java
@@ -16,7 +16,7 @@ public class Obstacle {
     }
 
     public void draw(TextGraphics graphics){
-        graphics.setForegroundColor(TextColor.Factory.fromString("#8B0000"));
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFAFA"));
         graphics.enableModifiers(SGR.BOLD);
         graphics.putString(new TerminalPosition(position.getX(), position.getY()), "O");
     }
