fdbe893113f411477e73bd2f12739aab96f5e6f1
Hugo Castro <hreisc@gmail.com>
2022-01-12 00:21:52 +0000

    font fail

--- spaceRace/src/main/java/Classes/Application.java
+++ spaceRace/src/main/java/Classes/Application.java
@@ -1,9 +1,11 @@
 package Classes;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Application {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException, URISyntaxException, FontFormatException {
         Game game = new Game(120,40);
         game.run();
     }
--- spaceRace/src/main/java/Classes/Game.java
+++ spaceRace/src/main/java/Classes/Game.java
@@ -2,19 +2,29 @@ package Classes;
 
 import Classes.Display;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Game {
     Screen screen = null;
     int width;
     int height;
+    //int fps;
     public Display display;
 
     public Game(int x, int y) {
@@ -22,18 +32,6 @@ public class Game {
         height = y;
         display = new Display(x, y);
 
-        try {
-            TerminalSize terminalSize = new TerminalSize(width, height);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
         public void processKey (KeyStroke key) {
@@ -63,7 +61,17 @@ public class Game {
             screen.refresh();
         }
 
-        public void run () throws IOException {
+        public void run () throws IOException, FontFormatException, URISyntaxException {
+
+            TerminalSize terminalSize = new TerminalSize(width, height);
+
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
             while (true) {
                 draw();
                 KeyStroke key = screen.readInput();
